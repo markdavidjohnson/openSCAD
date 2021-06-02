0738fa344146b576a319dd615966b45117787afb
markdavidjohnson <31580931+markdavidjohnson@users.noreply.github.com>
2021-06-02 09:42:50 -0600

    final version - needed a little post-process

--- saw_stop_for_depth_specific_bevel_cut_openSCAD.py
+++ saw_stop_for_depth_specific_bevel_cut_openSCAD.py
@@ -8,10 +8,27 @@ from shapeFunctions_openSCAD import trapezoidSymmetrical as tzd
 import shapeFunctions_openSCAD
 import math
 
+height = 25
+depth = 12
+sep = 20.5
+#sep =5
+part = resize([depth,sep,height])(cube())
+thkx = 3
+gap =16.2
+width = 5
 
+clips = []
+clips.append(resize([depth,width,thkx])(cube()))
+clips.append(resize([depth,width,thkx])(cube()))
+for i in range(len(clips)):
+    clips[i] = back(width-1)(clips[i])
+    angle_to_eachother = 7
+    angle = -angle_to_eachother*i + angle_to_eachother/2
+    clips[i] = rotate([-angle,0,0])(clips[i])
+    clips[i] = up((thkx + gap)*i)(clips[i])
 
-
-part = resize([25,10,20.5])(cube())
+for i in clips:
+    part += i
 to_print = part
 #to_print = receiver# + pipe #+ nosebase
 
